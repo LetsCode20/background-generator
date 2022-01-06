3fd388aad5818e395726ee6b4ef813f2bb302696
devtwins0 <devtwins0@gmail.com>
2022-01-06 14:50:56 +0100

    Start Background Linear Gradient Generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './app.css';
 
 function App() {
+  const [color, setColor] = useState('#ff0905');
+  const [color1, setColor1] = useState('#fe76fe');
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+    setColor(e.target.value);
+  };
+
+  const handleChange1 = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+    setColor1(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h2>Background Generator</h2>
+
+      <div className='container inputs'>
+        <input type='color' value={color} onChange={handleChange} />
+        <p>{color ? color : '#000000'}</p>
+
+        <input type='color' value={color1} onChange={handleChange1} />
+        <p>{color1 ? color1 : '#000000'}</p>
+      </div>
+
+      <div
+        className='container result'
+        style={{
+          background: `linear-gradient(to bottom, ${color}, ${color1})`,
+        }}
+      ></div>
     </div>
   );
 }
